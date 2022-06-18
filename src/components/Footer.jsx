8b0485521a56866fe57d80f444ff143fc36fa311
HMA <hensonlao@gmail.com>
2022-06-18 18:10:43 +0800

    First commit, headhunting-site project.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h1>This is Adonis the Footer</h1>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1>This is Miguel the Header</h1>
+        </header>
+    )
+}
+
+export default Header;
