7c468390b8ea58269c933702bfc7865b6b3980aa
henson <hensonlao@gmail.com>
2022-06-21 10:26:47 +0800

    Svgkadabraaa

--- src/components/Body/MainPageBanner.jsx
+++ src/components/Body/MainPageBanner.jsx
@@ -11,7 +11,7 @@ function MainPageBanner() {
         </section>
         <section className='main-banner-section-2'>
             <div>
-              <img src="../../../../img-rsr/svg-1.svg" alt="A man thinking for the future." />
+              <img src="src\components\imgrsr\svg1.svg" alt="sdff" />
             </div>
         </section>
     </div>
