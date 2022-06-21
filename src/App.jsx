73f8d5fed01ec6def11f14b8dbec8efcfe699a05
henson <hensonlao@gmail.com>
2022-06-21 08:06:30 +0800

    Some unfinished designs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Body/Header';
+import MainPageBanner from './components/Body/MainPageBanner';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <MainPageBanner/>
     </div>
   )
 }
---
+++ src/components/Body/MainPageBanner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const MainPageBanner = () => {
+  return (
+    <div className='main-banner'>
+        <section className='main-banner-section-1'>
+            <h1>
+              We are the global<br />services provider for your<br />
+              external workforce.
+            </h1>
+        </section>
+        <section className='main-banner-section-2'>
+            <h1>fdsfgdfsd</h1>
+        </section>
+    </div>
+  )
+}
+
+export default MainPageBanner;
