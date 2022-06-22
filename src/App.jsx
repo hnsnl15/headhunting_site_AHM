0bee2fe93ec5ec645a248f4b1f48c7db772b4527
henson <hensonlao@gmail.com>
2022-06-22 17:55:34 +0800

    New file added.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,8 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/Body/Header';
 import Login from './components/Login';
 import Register from './components/Register';
-<<<<<<< HEAD
 import FrontPage from './components/Body/FrontPage';
-=======
-import MainPageBanner from './components/Body/MainPageBanner';
 import Settings from './components/Settings';
->>>>>>> 3b374d7321347de84fbd9c2d87fc3830e0fb9471
 
 function App() {
   return (
