36a6bfec9578108bb1d56ef7b1563c40cac6b1fc
henson <hensonlao@gmail.com>
2022-06-22 22:38:54 +0800

    New section added in Mission.jsx

--- src/components/Front Page/Mission.jsx
+++ src/components/Front Page/Mission.jsx
@@ -6,7 +6,7 @@ function Mission() {
     <>
     <section className='our-mission'>
         <figure className='our-mission-figure-1'>
-            <SolutionSvg/>
+            <SolutionSvg />
         </figure>
 
         <section className='our-mission-text'>
@@ -23,29 +23,34 @@ function Mission() {
       <section className='mission-cards'>
 
         <div className='div-mission-1'>
-          <h2><span>People</span> Management</h2>
-            <p>We help companies to build their community of external talents and these talents to integrate into teams. Our goals is to break down barriers between internal talent and external talent.</p>
-        
+          <div>
+            <h2><span>People</span> Management</h2>
+              <p>We help companies to build their community of external talents and these talents to integrate into teams. Our goals is to break down barriers between internal talent and external talent.</p>
+          </div>
             <figure>
               <img src='https://raw.githubusercontent.com/hnsnl15/headhunting_site_AHM/master/src/components/imgrsr/Peoplemanagemnt.svg' alt="" />
             </figure>
         </div>
 
         <div className='div-mission-2'>
-          <h2><span>Project</span> Management</h2>
-          <p>We follow up on all projects carried out by external talents. We rely on out innovative digital solutions. We are committed to delivering.</p>
+          <div>
+              <h2><span>Project</span> Management</h2>
+              <p>We follow up on all projects carried out by external talents. We rely on out innovative digital solutions. We are committed to delivering.</p>
+          </div>
 
           <figure>
-            <img src='https://raw.githubusercontent.com/hnsnl15/headhunting_site_AHM/b38f2ec1e70fe357d6c59fdc34fb1e31397d1cf0/src/components/imgrsr/Payment.svg' alt="" />
+            <img src='https://raw.githubusercontent.com/hnsnl15/headhunting_site_AHM/master/src/components/imgrsr/Projectmanagement.svg' alt="" />
           </figure>
         </div>
   
         <div className='div-mission-3'>
-          <h2><span>Payment</span> Management</h2>
-          <p>We develop innovative and secure supplier invoicing, umbrella employment and payroll management solutions for our key account cutomenrs and for our freelance partners. We ensure fair remuneration for external talents.</p>
+          <div>
+            <h2><span>Payment</span> Management</h2>
+            <p>We develop innovative and secure supplier invoicing, umbrella employment and payroll management solutions for our key account cutomenrs and for our freelance partners. We ensure fair remuneration for external talents.</p>
+          </div>
         
           <figure>
-            <img src='' alt="" />
+            <img src='https://raw.githubusercontent.com/hnsnl15/headhunting_site_AHM/master/src/components/imgrsr/Payment.svg' alt="" />
           </figure>
         </div>
   
