3b374d7321347de84fbd9c2d87fc3830e0fb9471
adonisjr <adonisjr.suico@gmail.com>
2022-06-22 17:03:54 +0800

    added Settings link

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Body/Header';
 import Login from './components/Login';
 import Register from './components/Register';
 import MainPageBanner from './components/Body/MainPageBanner';
+import Settings from './components/Settings';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<MainPageBanner />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/settings' element={<Settings />} />
       </Routes>
     </Router>
   )
